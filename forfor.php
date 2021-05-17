c49d1c0d20e5dccb63f84190e45b48b91bb90108
glitchrent <glitchrent@gmail.com>
2021-05-17 16:22:18 +0700

    for loop if else
    learinnn

---
+++ forfor.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Format</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
+    <body>
+        <?php
+            for ($i=2; $i<=5; $i++){
+                echo "รอบใหญ่ที่ $i <br>";
+                for ($j=1; $j<3; $j++){
+                    echo " รอบเล็กที่ $j ($i*$j) ,";
+                }   // End for รอบเล็ก
+                echo "<br><br>";
+            } //End for รอบใหญ่
+        ?>
+    </body>
+    
+</html>
---
+++ intval.php
@@ -0,0 +1,28 @@
+<?php
+    //intaval
+    $sum ='35abc';
+    $x = 32;
+    $y = 5;
+    echo intaval(42);           echo '<br>';
+    echo intaval(4.2);          echo '<br>';
+    echo intaval('42');         echo '<br>';
+    echo intaval('+42');        echo '<br>';
+    echo intaval('-42');        echo '<br>';
+    echo intaval($sum);         echo '<br>';
+    echo intaval($x/$y);        echo '<br>';
+    echo intaval($x%$y);        echo '<br>';
+    $x = int(100.65161);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Format</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
+    <body>
+
+    </body>
+    
+</html>
